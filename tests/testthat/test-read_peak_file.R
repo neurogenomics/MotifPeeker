ba21145b21bb036e34d669ba04c40cdf95b7940d
HDash <16350928+HDash@users.noreply.github.com>
2024-06-21 21:24:40 +0100

    Add tests for accepting GRanges object

--- tests/testthat/test-read_peak_file.R
+++ tests/testthat/test-read_peak_file.R
@@ -9,6 +9,7 @@ test_that("read_peak_file can read MACS3 peak file and auto-infer format", {
                                    package = "MotifPeeker")
     macs3_peak_read <- read_peak_file(macs3_peak_file)
     expect_length(macs3_peak_read, 209)
+    expect_identical(read_peak_file(macs3_peak_read), macs3_peak_read)
 })
 test_that("read_peak_file fails with unknown file format forced", {
     invalid_peak_file <- system.file("extdata", "motif_MA1102.3.jaspar",
@@ -16,4 +17,10 @@ test_that("read_peak_file fails with unknown file format forced", {
     expect_error(read_peak_file(invalid_peak_file, file_format = "narrowPeak"))
     expect_error(read_peak_file(invalid_peak_file, file_format = "bed"))
     expect_error(read_peak_file(invalid_peak_file))
+})
+test_that("read_peak_file fails if GRanges without summit col is passed", {
+    macs3_peak_file <- system.file("extdata", "CTCF_ChIP_peaks.narrowPeak",
+                                   package = "MotifPeeker")
+    peak_gr <- rtracklayer::import(macs3_peak_file, format = "narrowPeak")
+    expect_error(read_peak_file(peak_gr))
 })
