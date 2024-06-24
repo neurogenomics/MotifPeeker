9d3c1ee0b6fe51118c1253bd155b5bdebea51a16
HDash <16350928+HDash@users.noreply.github.com>
2024-06-24 12:55:32 +0100

    Add report_command

---
+++ R/report_command.R
@@ -0,0 +1,43 @@
+#' Report command
+#' 
+#' Reconstruct the \code{\link{MotifPeeker}} command from the parameters used to
+#' generate the HTML report.
+#' 
+#' @param params A list of parameters used to generate the HTML report.
+#' 
+#' @examples
+#' report_command(params = list(
+#'    alignment_files = c("file1.bam", "file2.bam"),
+#'    exp_labels = c("exp1", "exp2"),
+#'    genome_build = "hg19"))
+#' 
+#' 
+#' @export
+report_command <- function(params) {
+    tab_spaces <- paste(rep(" ", nchar("MotifPeeker)")), collapse = "")
+    
+    params_values <- vapply(names(params), function(name) {
+        value <- params[[name]]
+        if (is.null(value)) {
+            value <- "NULL"
+        } else if (is.character(value)) {
+            if (length(value) > 1) {
+                value <- paste0("list(", 
+                                paste0('"', value, '"', collapse = ", "), ")")
+            } else {
+                value <- paste0('"', value, '"')
+            }
+        } else if (is.logical(value)) {
+            value <- ifelse(value, "TRUE", "FALSE")
+        } else if (!is.numeric(value)) {
+            value <- "<user_supplied>"
+        }
+        paste0(name, " = ", value)
+    }, character(1)) |>
+        paste(collapse = paste0(",\n", tab_spaces))
+    
+    cmd <- paste("MotifPeeker(", params_values, ")", sep = "")
+    cmd <- paste0("<pre><code class='language-r'>",cmd,"</code></pre>")
+    
+    return(cmd)
+}
---
+++ tests/testthat/test-report_command.R
@@ -0,0 +1,24 @@
+test_that("report_command works", {
+    params <- list(
+        peak_files = list("test", "test2"),
+        reference_index = 1,
+        alignment_files = NULL,
+        exp_labels = c("A", "B", "C"),
+        exp_type = NULL,
+        genome_build = NULL,
+        motif_files = motif,
+        motif_labels = NULL,
+        cell_counts = NULL,
+        denovo_motif_discovery = NULL,
+        denovo_motifs = 3,
+        motif_db = NULL,
+        download_buttons = NULL,
+        output_dir = NULL,
+        use_cache = NULL,
+        ncpus = NULL,
+        debug = FALSE,
+        verbose = TRUE
+    )
+    cmd <- report_command(params)
+    expect_true(grepl("MotifPeeker\\(", cmd))
+})
