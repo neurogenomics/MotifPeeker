00b3440ec14487cb33603a2562d47dd3b7d5af4d
HDash <16350928+HDash@users.noreply.github.com>
2024-07-01 16:55:21 +0100

    Add more tests

--- tests/testthat/test-MotifPeeker.R
+++ tests/testthat/test-MotifPeeker.R
@@ -16,12 +16,12 @@ test_that("MotifPeeker produces output files", {
                     package = "MotifPeeker")
     )
     
-    # alignments <- list(
-    #     system.file("extdata", "CTCF_ChIP_alignment.bam",
-    #                 package = "MotifPeeker"),
-    #     system.file("extdata", "CTCF_TIP_alignment.bam",
-    #                 package = "MotifPeeker")
-    # )
+    alignments <- list(
+        system.file("extdata", "CTCF_ChIP_alignment.bam",
+                    package = "MotifPeeker"),
+        system.file("extdata", "CTCF_TIP_alignment.bam",
+                    package = "MotifPeeker")
+    )
     
     motifs <- list(
         system.file("extdata", "motif_MA1930.2.jaspar",
@@ -30,10 +30,11 @@ test_that("MotifPeeker produces output files", {
                     package = "MotifPeeker")
     )
     
+    ## All optional features enabled
     output_dir <- MotifPeeker(
         peak_files = peaks,
         reference_index = 1,
-        alignment_files = NULL,
+        alignment_files = alignments,
         exp_labels = c("ChIP", "TIP"),
         exp_type = c("chipseq", "tipseq"),
         genome_build = "hg38",
@@ -50,6 +51,51 @@ test_that("MotifPeeker produces output files", {
         debug = TRUE,
         verbose = TRUE
     )
+    expect_true(file.exists(file.path(output_dir, "MotifPeeker.html")))
     
+    ## No alignment and motif files
+    output_dir <- MotifPeeker(
+      peak_files = peaks,
+      reference_index = 1,
+      alignment_files = NULL,
+      exp_labels = c("ChIP", "TIP"),
+      exp_type = c("chipseq", "tipseq"),
+      genome_build = "hg38",
+      motif_files = NULL,
+      motif_labels = NULL,
+      cell_counts = NULL,
+      denovo_motif_discovery = TRUE,
+      denovo_motifs = 3,
+      motif_db = NULL,
+      download_buttons = TRUE,
+      output_dir = tempdir(),
+      use_cache = TRUE,
+      display = NULL,
+      debug = TRUE,
+      verbose = TRUE
+    )
+    expect_true(file.exists(file.path(output_dir, "MotifPeeker.html")))
+    
+    ## Single set of files
+    output_dir <- MotifPeeker(
+      peak_files = peaks[1],
+      reference_index = 1,
+      alignment_files = alignments[1],
+      exp_labels = c("ChIP"),
+      exp_type = c("chipseq"),
+      genome_build = "hg38",
+      motif_files = motifs[[1]],
+      motif_labels = NULL,
+      cell_counts = NULL,
+      denovo_motif_discovery = TRUE,
+      denovo_motifs = 3,
+      motif_db = NULL,
+      download_buttons = TRUE,
+      output_dir = tempdir(),
+      use_cache = TRUE,
+      display = NULL,
+      debug = TRUE,
+      verbose = TRUE
+    )
     expect_true(file.exists(file.path(output_dir, "MotifPeeker.html")))
 })
